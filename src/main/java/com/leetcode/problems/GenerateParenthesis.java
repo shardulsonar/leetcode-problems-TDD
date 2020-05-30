615f9bfa12b32e4efe46df52d31dfe87bf27e445
Shardulsonar <shardulsonar@gmail.com>
2020-05-30 23:34:22 +0200

    Adding GenerateParenthesis code (Green)

--- src/main/java/com/leetcode/problems/GenerateParenthesis.java
+++ src/main/java/com/leetcode/problems/GenerateParenthesis.java
@@ -5,7 +5,31 @@ import java.util.List;
 
 public class GenerateParenthesis {
 
-    public List<String> generateAllPossibleParenthesis(int i) {
-        return new ArrayList<>();
+    public List<String> generateAllPossibleParenthesis(int num) {
+
+        String initialValue = "";
+        int numberOfOpenBrackets = 0;
+        int numberOfCloseBrackets = 0;
+        List<String> solution = new ArrayList<>();
+        generateParenthesisRecursively(initialValue,numberOfOpenBrackets ,numberOfCloseBrackets, num, solution);
+        return solution;
+    }
+
+    private void generateParenthesisRecursively(String currentString, int numberOfOpenBrackets, int numberOfCloseBrackets, int num, List<String> solution) {
+        if(numberOfOpenBrackets + numberOfCloseBrackets == 2*num){
+            solution.add(currentString);
+            return;
+        }
+
+        if(numberOfCloseBrackets > numberOfOpenBrackets){
+            return;
+        }
+        if(numberOfOpenBrackets < num){
+            generateParenthesisRecursively(currentString + "(", numberOfOpenBrackets + 1, numberOfCloseBrackets, num, solution);
+        }
+
+        if(numberOfOpenBrackets > numberOfCloseBrackets){
+            generateParenthesisRecursively(currentString + ")", numberOfOpenBrackets, numberOfCloseBrackets + 1, num, solution);
+        }
     }
 }
