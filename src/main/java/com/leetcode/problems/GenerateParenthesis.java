ab610bd679efac559d73eb80e0617641029c1a2a
Shardulsonar <shardulsonar@gmail.com>
2020-05-30 22:57:22 +0200

    Adding GenerateParenthesis Test (Red)

--- src/main/java/com/leetcode/problems/GenerateParenthesis.java
+++ src/main/java/com/leetcode/problems/GenerateParenthesis.java
@@ -1,5 +1,11 @@
 package com.leetcode.problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateParenthesis {
 
+    public List<String> generateAllPossibleParenthesis(int i) {
+        return new ArrayList<>();
+    }
 }
--- src/test/java/com/leetcode/problems/GenerateParenthesisTest.java
+++ src/test/java/com/leetcode/problems/GenerateParenthesisTest.java
@@ -1,10 +1,26 @@
 package com.leetcode.problems;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GenerateParenthesisTest {
 
     @Test
     void testGenerateParenthesis() {
+        GenerateParenthesis generateParenthesis = new GenerateParenthesis();
+        int n = 3;
+        List<String> solution = generateParenthesis.generateAllPossibleParenthesis(3);
+
+        List<String> expectedSolution = new ArrayList<>();
+        expectedSolution.add("((()))");
+        expectedSolution.add("(()())");
+        expectedSolution.add("(())()");
+        expectedSolution.add("()(())");
+        expectedSolution.add("()()()");
+
+        Assertions.assertEquals(solution, expectedSolution);
     }
 }
