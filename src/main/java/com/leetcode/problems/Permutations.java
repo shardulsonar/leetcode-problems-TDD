c922e1285e633dd06f0507fd9d92bbef8ca38c12
Shardulsonar <shardulsonar@gmail.com>
2020-06-06 15:31:54 +0200

    Adding adding code for Permutations

--- src/main/java/com/leetcode/problems/Permutations.java
+++ src/main/java/com/leetcode/problems/Permutations.java
@@ -1,4 +1,12 @@
 package com.leetcode.problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        return list;
+    }
 }
